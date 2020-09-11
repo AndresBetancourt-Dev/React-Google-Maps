e452d52e70910afa5e92c5b7cdaf1aabfc0fac9c
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-09-11 16:36:57 -0500

    Init React Google Maps on the Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, Marker, withScriptjs, withGoogleMap } from "react-google-maps";
+
+function Map(props){
+  return (
+  <GoogleMap defaultZoom={10}
+  defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+  <Marker key={1} position={{lat: -34.397 , lng: 150.640}}/>
+  <Marker key={2} position={{lat: -34.396 , lng: 150.630}}/>
+  <Marker key={3} position={{lat: -34.395 , lng: 150.620}}/>
+  <Marker key={4} position={{lat: -34.394 , lng: 150.610}}/>
+  <Marker key={5} position={{lat: -34.393 , lng: 150.600}}/>
+  <Marker key={6} position={{lat: -34.392 , lng: 150.549}}/>
+
+  </GoogleMap>
+  )
+}
+ 
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyATL0fkxblSCgXaN06zZ8qAbGES4GTILwg`} loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `100%` }} />}
+  mapElement={<div style={{ height: `100%` }} />}/>
     </div>
   );
 }
